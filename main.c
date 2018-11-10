ada82373cf61044784c6c1667d9a83d38a9ac596
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2018-11-10 14:23:54 -0600

    Restore files because of wrong commit

---
+++ main.c
@@ -0,0 +1,72 @@
+/**
+	\file
+	\brief
+		This project shows how works the emulation of a VT 100 in teraterm.
+	\author J. Luis Pizano Escalante, luispizano@iteso.mx
+	\date	27/07/2015
+	\todo
+		To implement the UART_init function
+ */
+
+#include <UART.h>/**UART device driver*/
+#include "MK64F12.h" /* include peripheral declarations */
+#include "NVIC.h"/**NVIC device driver*/
+#include "stdio.h"
+
+
+/**This is mail box to received the information from the serial port*/
+extern UART_MailBoxType UART0_MailBox;
+
+int main(void)
+{
+	/**Enables the clock of PortB in order to configures TX and RX of UART peripheral*/
+	SIM->SCGC5 = SIM_SCGC5_PORTB_MASK;
+	/**Configures the pin control register of pin16 in PortB as UART RX*/
+	PORTB->PCR[16] = PORT_PCR_MUX(3);
+	/**Configures the pin control register of pin16 in PortB as UART TX*/
+	PORTB->PCR[17] = PORT_PCR_MUX(3);
+	/**Configures UART 0 to transmit/receive at 11520 bauds with a 21 MHz of clock core*/
+	UART_init (UART_0,  21000000, BD_115200);
+		//printf("UART is configured");
+
+	NVIC_enableInterruptAndPriotity(UART0_IRQ, PRIORITY_10);
+		/**The following sentences send strings to PC using the UART_putString function. Also, the string
+
+	/**Enables the UART 0 interrupt*/
+	UART_interruptEnable(UART_0);
+	/**Enables the UART 0 interrupt in the NVIC*/
+
+		/**Enables interrupts*/
+	EnableInterrupts;
+
+	/** VT100 command for text in red and background in cyan*/
+	UART_putString(UART_0,"\033[0;32;46m");
+	/*VT100 command for clearing the screen*/
+	UART_putString(UART_0,"\033[2J");
+	/** VT100 command for text in red and background in green*/
+	UART_putString(UART_0,"\033[0;32;41m");
+	/** VT100 command for positioning the cursor in x and y position*/
+	UART_putString(UART_0,"\033[10;10H");
+	UART_putString(UART_0, "Micros y DSPs\r");
+	/** VT100 command for positioning the cursor in x and y position*/
+	UART_putString(UART_0,"\033[11;10H");
+	UART_putString(UART_0, "    ITESO\r");
+	/** VT100 command for positioning the cursor in x and y position*/
+	UART_putString(UART_0,"\033[12;10H");
+
+
+
+	for(;;) {
+
+		if(UART0_MailBox.flag)
+			{
+				/**Sends to the PCA the received data in the mailbox*/
+				UART_putChar (UART_0, UART0_MailBox.mailBox);
+
+				/**clear the reception flag*/
+				UART0_MailBox.flag =0;
+			}
+	}
+
+	return 0;
+}
